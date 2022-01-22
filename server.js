44d2c7e8ad4a79be67e7461850151ff00db64374
patthekid99 <pattyvan4@gmail.com>
2022-01-22 12:22:14 -0800

    Add working build

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ require('dotenv').config()
 const express = require('express')
 const multer = require('multer')
 const fs = require('fs')
+const path = require("path")
 
 const mysqlDatabase = require('./database')
 
 const app = express()
 
 const upload = multer({ dest: 'images/' })
+
+app.use(express.static(path.join(__dirname, "build")))
 
 app.get('/images/:imageName', (req, res) => {
     const imageName = req.params.imageName
