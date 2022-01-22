0f51340e0ec0453af7d490c8499f604b07d36df6
patthekid99 <pattyvan4@gmail.com>
2022-01-22 12:15:19 -0800

    Add working backend

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+
 const express = require('express')
-const fs = require('fs')
 const multer = require('multer')
+const fs = require('fs')
+
+const mysqlDatabase = require('./database')
 
 const app = express()
 
@@ -10,15 +14,20 @@ app.get('/images/:imageName', (req, res) => {
     const imageName = req.params.imageName
     const readStream = fs.createReadStream(`images/${imageName}`)
     readStream.pipe(res)
+  })
+
+app.get('/api/images', async (req, res) => {
+    const images = await mysqlDatabase.getImages()
+    res.send({images})
 })
 
-
-app.post('/api/images', upload.single('image'), (req, res) => {
+app.post('/api/images', upload.single('image'), async (req, res) => {
     const imagePath = req.file.path
     const description = req.body.description
 
-    console.log(description, imagePath)
-    res.send({ description, imagePath })
+    const image = await mysqlDatabase.addImage(imagePath, description)
+    
+    res.send({image})
 })
 
 app.listen(8080, () => console.log("listening on port 8080"))
